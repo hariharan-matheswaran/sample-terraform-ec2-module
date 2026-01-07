f04154007799b175ea92c543bc696cfc1bb1fb9d
hariharan-matheswaran <harimalar99m@gmail.com>
2026-01-08 01:42:44 +0530

    Fix formatting of AMI ID in main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws"{
 }
 
 resource "aws_instance" "myec2"{
-    ami = ami-068c0051b15cdb816
+    ami = "ami-068c0051b15cdb816"
     instance_type = "t3.micro"
     tags={
         "Env" = "Prod"
